7e1fe24b4b36754e896e30a8dc6b90657aa83a56
Alberto <alberto.moreno2357@gmail.com>
2024-03-13 20:16:05 +0100

    feat: ended react web

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Features, SectionWrapper, Download} from "./components";
 import assets from './assets'
+import styles from "./styles/Global";
 
 const App = () => {
   return (
@@ -11,6 +12,29 @@ const App = () => {
         mockupImg={assets.homeHero}
         banner="banner"
       />
+      <SectionWrapper 
+        title="Smart User Interface Marketplace"
+        description="Experience a buterry UI of ProNef Marketplace. Smooth constant colors of a fluent UI design."
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+      <SectionWrapper 
+        title="Deployment"
+        description="ProNef is built using Expo which runs on all user's devices. You can easily get your app into peoples hands."
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper 
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFTs while the second one shows the details on a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>Made by {" "} <span className="bold">Alberto Moreno</span></p>
+      </div>
     </>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,18 @@
 
-const Button = () => {
+import styles from '../styles/Global'
+
+const Button = ({ assetUrl, link }) => {
   return (
-    <div>Button</div>
+    <div
+      className={styles.btnBlack}
+      onClick={() => window.open(link, "_blank")}
+    >
+      <img className={styles.btnIcon} src={assetUrl} alt="expo_icon" />
+      <div className='flex flex-col justify-start ml-4'>
+        <p className={`${styles.btnText} font-normal text-xs`}>View it on</p>
+        <p className={`${styles.btnText} font-bold text-sm`}>Expo Store</p>
+      </div>
+    </div>
   )
 }
 
